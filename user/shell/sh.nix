112f3de0b9b7a85348ee4349e7af9156f6dc7c73
SpiderMonkey <ascenzen11@gmail.com>
2024-11-03 09:54:50 -0600

    zsh

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -31,13 +31,8 @@ in
     initExtra = ''
       # Zoxide initialization
       eval "$(zoxide init zsh)"
-      
-      PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
-       %F{green}→%f "
-      RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
-      [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
-      bindkey '^P' history-beginning-search-backward
-      bindkey '^N' history-beginning-search-forward
+export ZSH="/home/$(whoami)/.oh-my-zsh"
+
     '';
   };
 
