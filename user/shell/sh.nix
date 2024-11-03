b0e90df84422de422bd3a93f39a53f4443760e7b
SpiderMonkey <ascenzen11@gmail.com>
2024-11-03 01:56:33 -0600

    zsh autosuggestion

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -24,7 +24,7 @@ in
     enable = true;
 
 
-autosuggestion.enable
+    enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
     enableCompletion = true;
     shellAliases = myAliases;
