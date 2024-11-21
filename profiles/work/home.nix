60cc523b7c795c949f3095acc48c4a343b51ab6d
SpiderMonkey <ascenzen11@gmail.com>
2024-11-21 15:53:06 +0530

    aditya

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -28,7 +28,7 @@
               ../../user/hardware/bluetooth.nix # Bluetooth
             ];
 
-  home.stateVersion = "24.05"; # Please read the comment before changing.
+  home.stateVersion = "22.11"; # Please read the comment before changing.
 
   home.packages = (with pkgs; [
     # Core
